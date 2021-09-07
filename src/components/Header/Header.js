12248d7517a4f6e9fcd01bcacd0f7232e98c3660
ramazonHub <ramazon2021@gmail.com>
2021-09-07 11:32:07 +0500

    finish

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,28 +3,34 @@ import { NavLink } from "react-router-dom";
 
 function Header(){
     return(
-        <header className="header ">
-            <nav className="header-nav container">
-                <ul className="nav-list">
-                    <li className="nav-list__item">
-                        <NavLink to="/" className="nav-list__link" activeClassName="nav-list__link-active" exact >Home</NavLink>
-                    </li>
-                    <li className="nav-list__item">
-                        <NavLink to="/about" className="nav-list__link" activeClassName="nav-list__link-active" exact >About me</NavLink>
-                    </li>
-                    <li className="nav-list__item">
-                        <NavLink to="/work" className="nav-list__link" activeClassName="nav-list__link-active" >Work</NavLink>
-                    </li>
-                    {/* <li className="nav-list__item">
-                        <NavLink to="/writing" className="nav-list__link" activeClassName="nav-list__link-active" >My blogs</NavLink>
-                    </li> */}
-                    <li className="nav-list__item">
-                        <NavLink to="/contact" className="nav-list__link" activeClassName="nav-list__link-active" >Contact</NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-
+        <div className="header ">
+            <header className="header__content container">
+                <nav className="header-nav ">
+                    <ul className="nav-list">
+                        <li className="nav-list__item">
+                            <NavLink to="/" className="nav-list__link" activeClassName="nav-list__link-active" exact >Home</NavLink>
+                        </li>
+                        <li className="nav-list__item">
+                            <NavLink to="/about" className="nav-list__link" activeClassName="nav-list__link-active" exact >About me</NavLink>
+                        </li>
+                        <li className="nav-list__item">
+                            <NavLink to="/work" className="nav-list__link" activeClassName="nav-list__link-active" >Work</NavLink>
+                        </li>
+                        {/* <li className="nav-list__item">
+                            <NavLink to="/writing" className="nav-list__link" activeClassName="nav-list__link-active" >My blogs</NavLink>
+                        </li> */}
+                        <li className="nav-list__item">
+                            <NavLink to="/contact" className="nav-list__link" activeClassName="nav-list__link-active" >Contact</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <div className="menu">
+                <div className="menu__line"></div>
+                <div className="menu__line"></div>
+                <div className="menu__line"></div>
+            </div>
+        </div>
     )
 }
 export default Header
--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -10,7 +10,7 @@ const HomePage = () => {
                     <h1 className="home-hero__title home-hero__job">Men Frontend dasturchisiman.</h1>
                     <p className="home-hero__text home-hero__work ">Hozirga qadar real loyihalarda qatnashmaganman ammo qo'limdan ko'p ishlar keladi.Keling vaqtni behuda ketkazmaylik bu yerda mening qilgan loyihalarim:<Link to="/work" className="home-hero__link">My projects</Link></p>
                     <p className="home-hero__text home-hero__life ">Haqiqiy hayotda yaxshi suhbatdosh,qiziquvchan, o'z ishini yaxshi ko'ruvchi insonman.Bu yerda men haqimda ko'proq : <Link to="/about" className="home-hero__link">About me</Link> ✌️</p>
-            </div>
+                 </div>
             </div>
         </>
      );
