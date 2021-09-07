456394e2d09401176691bad685b40e90608bfdd4
ramazonHub <ramazon2021@gmail.com>
2021-09-07 19:10:09 +0500

    finish

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -6,8 +6,11 @@ const HomePage = () => {
         <>
             <div className="home container">
                 <div className="home-hero ">
-                    <p className="home-hero__text home-hero__me">Salom mening ismim Ramazon</p>
-                    <h1 className="home-hero__title home-hero__job">Men Frontend dasturchiman.</h1>
+                    <p className="home-hero__text home-hero__me">Hi,I'm Ramazon</p>
+                    <h1 className="home-hero__title home-hero__job">I'm Frontent developer
+                    
+                    
+                    </h1>
                     <p className="home-hero__text">Mening biladigan texnologiyalarim</p>
                     <ul className="home-list">
                         <li className="home-list__item">Html5</li>
@@ -17,7 +20,7 @@ const HomePage = () => {
                         <li className="home-list__item">MaterialUI</li>
                         <li className="home-list__item">GitHub ( jamoaviy ishlash )</li>
                     </ul>
-                    <p className="home-hero__text home-hero__work ">Bu yerda mening qilgan loyihalarim:<Link to="/work" className="home-hero__link">My projects</Link></p>
+                    <p className="home-hero__text home-hero__work ">Bu yerda mening qilgan loyihalarim : <Link to="/work" className="home-hero__link">My projects</Link></p>
                     <p className="home-hero__text home-hero__life ">Haqiqiy hayotda yaxshi suhbatdosh,qiziquvchan, o'z ishini yaxshi ko'ruvchi insonman.Bu yerda men haqimda ko'proq : <Link to="/about" className="home-hero__link">About me</Link> ✌️</p>
                  </div>
             </div>
